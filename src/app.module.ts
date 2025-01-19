d3b3003a457b79bc2b7e69fb6053203e1c78f748
surinnnnnn <didthfls2@naver.com>
2025-01-19 15:35:07 +0900

    fix: .env 환경 변수명 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,7 +31,7 @@ const typeOrmModuleOptions = {
   useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
     namingStrategy: new SnakeNamingStrategy(),
     type: 'mysql',
-    username: configService.get('DB_USERNAME'),
+    username: configService.get('DB_USER'),
     password: configService.get('DB_PASSWORD'),
     host: configService.get('DB_HOST'),
     port: configService.get('DB_PORT'),
@@ -59,7 +59,7 @@ const typeOrmModuleOptions = {
       isGlobal: true,
       validationSchema: Joi.object({
         JWT_SECRET_KEY: Joi.string().required(),
-        DB_USERNAME: Joi.string().required(),
+        DB_USER: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.number().required(),
