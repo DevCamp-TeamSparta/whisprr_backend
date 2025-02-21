d5f95cf28bd68e4eb01a04c9ba6ae3458f48a70a
surinnnnnn <didthfls2@naver.com>
2025-02-22 00:48:55 +0900

    fix: 트랜잭션 제거하였다.

--- src/journal/journal.service.ts
+++ src/journal/journal.service.ts
@@ -7,7 +7,7 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JournalEntity } from './entities/journal.entity';
-import { Between, DataSource, EntityManager, LessThan, Repository } from 'typeorm';
+import { Between, LessThan, Repository } from 'typeorm';
 import { UserEntity } from '../user/entities/user.entity';
 import { Journal, OpenAiService } from '../open-ai/open-ai.service';
 import { ModifyJournalDto } from './dto/modify_journal.dto';
@@ -38,7 +38,6 @@ export class JournalService {
     private openAiService: OpenAiService,
     @InjectRepository(OriginalJournalEntity)
     private originalJournalRepository: Repository<OriginalJournalEntity>,
-    private dataSource: DataSource,
   ) {}
 
   //1. 저널 생성
@@ -64,65 +63,52 @@ export class JournalService {
     journal: Journal,
     date: Date,
   ): Promise<Partial<ReturnedJournal>> {
-    return await this.dataSource.transaction(async (manager) => {
-      const savedOriginal = await manager
-        .getRepository(OriginalJournalEntity)
-        .createQueryBuilder()
-        .insert()
-        .values({
-          title: journal.title,
-          keyword: journal.keyword,
-          content: journal.content,
-        })
-        .execute();
+    const journalRecord = {
+      title: journal.title,
+      keyword: journal.keyword,
+      content: journal.content,
+    };
 
-      const originalJournalId = savedOriginal.identifiers[0].id;
-      const createdAt = new Date();
-
-      await manager
-        .getRepository(JournalEntity)
-        .createQueryBuilder()
-        .insert()
-        .values({
-          user: { user_id: user.user_id },
-          originalJournal: { id: originalJournalId },
-          title: journal.title,
-          keyword: journal.keyword,
-          content: journal.content,
-          created_at: createdAt,
-          date: date,
-        })
-        .execute();
-
-      await this.updatejournalCreation(manager, user, date);
-
-      const jwtToken = await this.userService.updateWritingCount(manager, user); //UserService 5번
-
-      const returndJournal: Partial<ReturnedJournal> = {
-        title: journal.title,
-        keyword: journal.keyword,
-        content: journal.content,
-        date: date,
-        created_at: createdAt,
-        jwtToken,
-      };
-      return returndJournal;
+    const original = this.originalJournalRepository.create({
+      ...journalRecord,
     });
+
+    await this.originalJournalRepository.save(original);
+
+    const newJournal = this.journalRepository.create({
+      user: user,
+      ...journalRecord,
+      created_at: new Date(),
+      date: date,
+      originalJournal: original,
+    });
+
+    await this.journalRepository.save(newJournal);
+    await this.updatejournalCreation(user, date);
+
+    const jwtToken = await this.userService.updateWritingCount(user); //UserService 5번
+
+    const returndJournal: Partial<ReturnedJournal> = {
+      title: newJournal.title,
+      keyword: newJournal.keyword,
+      content: newJournal.content,
+      date: newJournal.date,
+      created_at: newJournal.created_at,
+      jwtToken,
+    };
+    return returndJournal;
   }
 
   //1.2 저널 생성 기록 생성
 
-  private async updatejournalCreation(manager: EntityManager, user: UserEntity, date: Date) {
-    return await manager
-      .getRepository(JournalCreationEntity)
-      .createQueryBuilder()
-      .insert()
-      .values({
-        user: user,
-        journal_date: date,
-        created_at: new Date(),
-      })
-      .execute();
+  private async updatejournalCreation(user: UserEntity, date: Date): Promise<void> {
+    const newRecord = this.journalCreationRepository.create({
+      user,
+      journal_date: date,
+      created_at: new Date(),
+    });
+
+    await this.journalCreationRepository.save(newRecord);
   }
 
   //2. 저널 목록 조회 (lastDate: 이전 요청 저널들 중 마지막 저널의 해당 날짜, limit: 저널 요청 개수)
