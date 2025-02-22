51094197de1f3c11b79556b6983fd5ed88382887
surinnnnnn <didthfls2@naver.com>
2025-02-22 11:42:07 +0900

    fix:journal 엔터티 수정하였다.

--- src/journal/entities/journal.entity.ts
+++ src/journal/entities/journal.entity.ts
@@ -51,5 +51,6 @@ export class JournalEntity {
 
   // 1:1 original_journal
   @OneToOne(() => OriginalJournalEntity, { cascade: true, onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'original_journal_id' })
   originalJournal: OriginalJournalEntity;
 }
